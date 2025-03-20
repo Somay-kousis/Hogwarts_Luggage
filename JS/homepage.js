c55782546ec63f077766345484de662fc0cc90ff
Somay <Somaykaush@gmail.com>
2025-03-21 02:20:12 +0530

    Initial commit

---
+++ JS/homepage.js
@@ -0,0 +1,43 @@
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const dots = document.querySelectorAll(".dot");
+
+const pages = ["Homepage.html", "ContactUs.html", "Purchases.html"];
+
+const skipIndex = 2;
+
+let currentPageIndex = 0;
+
+function updateActiveDot() {
+  dots.forEach((dot, index) => {
+    dot.classList.remove("active");
+
+    if (index === 0) {
+      dot.classList.add("active");
+    }
+  });
+}
+
+updateActiveDot();
+
+arrowLeft.addEventListener("click", () => {
+  window.location.href = pages[2];
+});
+
+arrowRight.addEventListener("click", () => {
+  window.location.href = pages[1];
+});
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    if (index === 0) {
+      return;
+    } else if (index === 1) {
+      window.location.href = "ContactUs.html";
+    } else if (index === skipIndex) {
+      return;
+    } else if (index === 3) {
+      window.location.href = "Purchases.html";
+    }
+  });
+});
